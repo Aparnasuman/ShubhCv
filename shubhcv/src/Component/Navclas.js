fce977b46336d8aa8cd8abb329ebe352687ea29f
Aparna Suman <Aparnasuman2@gmail.com>
2020-12-26 20:28:48 +0530

    img finish

--- shubhcv/src/Component/Navclas.js
+++ shubhcv/src/Component/Navclas.js
@@ -4,7 +4,7 @@ import { Navbar, Nav } from "react-bootstrap";
 class Navclas extends React.Component {
     render() {
         return (
-            <Navbar expand="lg">
+            <Navbar expand="lg" sticky="top" >
                 <Navbar.Brand href="#home">PROTOFILO</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
