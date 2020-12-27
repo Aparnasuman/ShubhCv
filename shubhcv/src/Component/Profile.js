d09590359d51138847e26c40504159c3627a9dc9
Aparna Suman <Aparnasuman2@gmail.com>
2020-12-27 18:13:25 +0530

    portfolio

--- shubhcv/src/Component/Profile.js
+++ shubhcv/src/Component/Profile.js
@@ -4,9 +4,29 @@ class Profiie extends React.Component {
     render() {
         return (
             <div className="section">
-                <div className="container">
-                    <h3 className="profil">PROFILE</h3>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6 profile">
+                            <h4 className="profil">PROFILE</h4>
+                            <p className="profil-a">Hello! I am Shubham Kumar, a Research Scholar in National Institute of Technology Karnataka, Surathkal.
+                            I enjoy creating things that live on the internet, whether it is a web application or mobile application. My goal is to create secure, scalable products with better performance.
+                            I did my Bachelors in Computer Science and Engineering from Lovely professional University and then opted for research at NITK.</p>
+                        </div>
+                        <div className="col-md-6">
+                            <div id="mainDiv">
+                                <div id="boxDiv">
+                                    <div id="front"></div>
+                                    <div id="back"></div>
+                                    <div id="left"></div>
+                                    <div id="right"></div>
+                                    <div id="top"></div>
+                                    <div id="bottom"></div>
 
+                                    <div class="shadow"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
