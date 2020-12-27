786a291303cfcf456b42712141710632a7210528
Shubham Kumar <sshubhamk1@hotmail.com>
2020-12-27 19:09:40 +0530

    navbar updated

--- shubhcv/src/Component/NavigationBar.js
+++ shubhcv/src/Component/NavigationBar.js
@@ -1,24 +1,23 @@
 import React from "react";
-import "./Navbar.css"
+import "./NavigationBar.css";
 import { Navbar, Nav } from "react-bootstrap";
-class Navclas extends React.Component {
-    render() {
-        return (
-            <Navbar expand="lg" sticky="top" >
-                <Navbar.Brand href="#home">PROTOFILO</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">HOME</Nav.Link>
-                        <Nav.Link href="#link">ABOUT</Nav.Link>
-                        <Nav.Link href="#link">WORKS</Nav.Link>
-                        <Nav.Link href="#link">BLOG</Nav.Link>
-                        <Nav.Link href="#link">CONTACT</Nav.Link>
-                    </Nav>
-
-                </Navbar.Collapse>
-            </Navbar>
-        )
-    }
+class NavigationBar extends React.Component {
+  render() {
+    return (
+      <Navbar expand="lg" sticky="top">
+        <Navbar.Brand href="#home">PROTOFILO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">HOME</Nav.Link>
+            <Nav.Link href="#about">ABOUT</Nav.Link>
+            <Nav.Link href="#works">WORKS</Nav.Link>
+            <Nav.Link href="#blog">BLOG</Nav.Link>
+            <Nav.Link href="#contact">CONTACT</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
-export default Navclas;
+export default NavigationBar;
